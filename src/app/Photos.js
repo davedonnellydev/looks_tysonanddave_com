fc178808241d2ecfe6e5e44b7fb6429f7213b704
Dave Buckley <davidpaulbuckley@gmail.com>
2023-06-20 23:37:02 +1000

    Created new react app with basic react image gallery and lightbox functionality (test images)

---
+++ src/app/Photos.js
@@ -0,0 +1,23 @@
+const photos = [
+   {
+   src: '/img/test1.jpeg',
+   width: 8206997084548106.0,
+   height: 10000000000000000,
+   },
+   {
+   src: '/img/test2.jpeg',
+   width: 6635294117647059.0,
+   height: 10000000000000000,
+   },
+   {
+   src: '/img/test3.jpeg',
+   width: 3550185873605947.5,
+   height: 10000000000000000,
+   },
+   {
+   src: '/img/test4.jpeg',
+   width: 6666666666666666.0,
+   height: 10000000000000000,
+   },
+]
+export default photos
